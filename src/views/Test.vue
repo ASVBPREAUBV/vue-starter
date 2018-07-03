f688cff84b1dbfa0b2d7cdc9cd5045554e2681f7
Paul Mayer <paul@paul-mayer.eu>
2018-07-03 13:47:29 +0200

    init ws, init mini test component

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,6 +1,32 @@
 <template>
-  <div class="test">
-    <h1>This is an Test page</h1>
-
-  </div>
+    <div>
+        <input v-model="test"></input>
+        <h2>test: {{test}}</h2>
+        <button @click="greet">greet</button>
+    </div>
 </template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+    @Component({
+        components: {
+            HelloWorld
+        }
+    })
+    class Test extends Vue {
+        test = '';
+
+        constructor() {
+            super()
+            this.test = "constructor"
+        }
+
+        greet() {
+            this.test = 'greet'
+        }
+    }
+
+    export default Test
+</script>
