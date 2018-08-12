70a8be2cf469177f2a89293fe64cd0ab888f4e9a
Paul Mayer <paul@paul-mayer.eu>
2018-08-12 22:13:16 +0200

    init login

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <button @click="test">greet</button>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component({
+        components: {}
+    })
+
+    class Login extends Vue {
+
+        test=()=>{
+            console.log('test')
+        }
+    }
+
+
+    export default Login;
+</script>
